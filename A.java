751462e982808a5082b09697e55867babe383ee4
fahmi200706 <fahmi.asyahdan@gmail.com>
2025-03-04 11:11:21 +0700

    Add files via upload
    Commit Abstract

--- A.java
+++ A.java
@@ -2,14 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package konstrukator;
+package Abstrack;
 
 /**
  *
  * @author DELL
  */
-public class A {
-    A(String param1){
-        System.out.println("KOnstruktor class A dengan Parameter " + param1 +"dieksekusi.....");
-    } 
+public abstract class A {
+    abstract public void  method2();
+    
+    public void method1(){
+        System.out.println("Method konkrit dari class A");
+    }
 }
