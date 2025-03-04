f4215ffefa0e1960251e7658ec9cdd0964d82f92
fahmi200706 <fahmi.asyahdan@gmail.com>
2025-03-04 07:54:34 +0700

    Create test.java
    first

---
+++ test.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pewarisan;
+
+/**
+ *
+ * @author DELL
+ */
+public class test {
+    public static void main(String[] args) {
+        // Membuat objek Pelajar
+        pelajar mahasiswa = new pelajar("Lukman", 172, 75, "1010651098", "UM JEMBER", 98.8);
+
+        // Menampilkan informasi objek Pelajar
+        System.out.println(mahasiswa.toString());
+    }
+}
